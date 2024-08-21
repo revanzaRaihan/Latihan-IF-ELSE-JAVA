d704052929d60e7baea7b49f17a9b2d11803eebe
Revanza Raihan Rizqullah <revanzaraihanrizqullah@gmail.com>
2024-08-21 11:34:21 +0800

    Update latihan1_if.java

--- latihan1_if.java
+++ latihan1_if.java
@@ -4,19 +4,17 @@ import java.util.Scanner;
 public class latihan1_if {
     public static void main(String[] args) {
         @SuppressWarnings("resource")
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Masukan Angka :");
+         System.out.print("Masukan Angka :");
         int angka = input.nextInt();
         if (angka != 0) {
             if (angka % 2 == 0) {
                 System.out.println("Genap");
             }
+            else{
+                System.out.println("Ganjil");
+            }
         }
-        else if (angka % 2 == 1) {
-            System.out.println("Ganjil");
-        }
-        if (angka == 0){
+        else if (angka == 0){
             System.out.println("NOL");
         }
     }
